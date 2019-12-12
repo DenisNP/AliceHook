c43b3a47c8750e75f242d7b2e1840409c0a5c74b
Denis Peshekhonov <denis@peshekhonov.ru>
2019-12-13 00:05:50 +0300

    capitalize fix

--- AliceHook/Utils.cs
+++ AliceHook/Utils.cs
@@ -13,6 +13,10 @@ namespace AliceHook
 
         public static string CapitalizeFirst(this string s)
         {
+            if (s.IsNullOrEmpty())
+            {
+                return "";
+            }
             return s.Substring(0, 1).ToUpper() + s.Substring(1);
         }
 
